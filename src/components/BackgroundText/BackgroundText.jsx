8dab7e519d5713759e2f4a1089144c344009510d
nedu'm <davidsonchinedum@gmail.com>
2022-05-07 21:53:05 +0100

    fixed responsive err

--- src/components/BackgroundText/BackgroundText.jsx
+++ src/components/BackgroundText/BackgroundText.jsx
@@ -3,16 +3,25 @@ import styled from 'styled-components';
 
 const BgTextContainer = styled.div`
 margin: 5rem 0.5rem 5rem 0.5rem;
-padding: 1rem 8rem 1rem 8rem;
+padding: 1rem 9rem 1rem 9rem;
 
+  @media screen and (max-width: ${props => props.theme.breakpoints.md}) {
+    margin-left: 1rem;
+    padding: 0;
+  }
 `;
 const BgTextContent = styled.h1`
-  font-size: 3.6rem;
+  font-size: clamp(3.2rem, 10vw, 4rem);
   font-weight: 500;
   font-family: "Outfit", sans-serif;
   color: #fff;
   text-align: center;
   line-height: 1.5;
+
+  @media screen and (max-width: ${props => props.theme.breakpoints.md}) {
+    font-size: 2em;
+    text-align: left;
+  }
 `;
 
 const BackgroundText = () => {
